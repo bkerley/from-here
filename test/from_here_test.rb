9d6f2b6ef1dc390f9713834f8e734c6026730698
Bryce Kerley <bkerley@brycekerley.net>
2009-05-27 19:07:48 -0400

    failing load test

--- test/from_here_test.rb
+++ test/from_here_test.rb
@@ -5,6 +5,7 @@ class FromHereTest < Test::Unit::TestCase
     should 'have a from_here method' do
       assert FromHere.respond_to?(:from_here)
     end
+
     context 'from_here method' do
       should 'fail without a block' do
 
@@ -13,6 +14,11 @@ class FromHereTest < Test::Unit::TestCase
         this_file = File.join(File.dirname(__FILE__), File.basename(__FILE__))
         found_file = FromHere.from_here{}
         assert_equal this_file, found_file
+      end
+
+      should 'find another file' do
+        load FromHere.from_here('fixtures','canary.rb')
+        assert_respond_to Canary, :tweet
       end
     end
   end
