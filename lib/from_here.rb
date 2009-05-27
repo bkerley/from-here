4348c9b3ead185a5d41a4340075dcbca126819d0
Bryce Kerley <bkerley@brycekerley.net>
2009-05-27 19:17:36 -0400

    raise an error to pass a test

--- lib/from_here.rb
+++ lib/from_here.rb
@@ -1,6 +1,12 @@
 module FromHere
+  class NoBlockError < StandardError
+    def initialize
+      super 'from_here requires a block (an empty block is okay)'
+    end
+  end
 
   def self.from_here(*path, &block)
+    raise NoBlockError unless block
     File.join(
               File.dirname(eval('__FILE__', block.binding)),
               *path
